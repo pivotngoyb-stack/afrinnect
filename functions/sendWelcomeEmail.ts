31f8caf24633ce2644e776a45cfbb7716ab19412
PIVOT NGOY <pivotngoyb@gmail.com>
2025-12-26 23:54:17 +0000

    File changes

--- functions/sendWelcomeEmail.ts
+++ functions/sendWelcomeEmail.ts
@@ -33,7 +33,7 @@ Deno.serve(async (req) => {
         The Afrinnect Team
         
         ---
-        Download the app: [Link]
+        Visit app: https://afrinnect-658a9066.base44.app
         Follow us: Instagram | Twitter | Facebook
       `
     });
