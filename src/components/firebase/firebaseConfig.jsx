8b1d79cc6bf0aaff0bf9bb6b94348ff3e5d4c89d
PIVOT NGOY <pivotngoyb@gmail.com>
2025-12-27 00:17:32 +0000

    File changes

--- src/components/firebase/firebaseConfig.jsx
+++ src/components/firebase/firebaseConfig.jsx
@@ -1,3 +1,4 @@
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export { app };
